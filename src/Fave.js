11e318360b0bcfb60d717e4e54177163c599280d
sameghosts <seanfeiner.brown@gmail.com>
2021-02-03 23:37:23 -0800

    fave component state with ternary for class change

--- src/Fave.js
+++ src/Fave.js
@@ -4,14 +4,25 @@ class Fave extends Component {
   constructor (props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      isFave: false
+    }
   }
   handleClick(e){
     console.log('handling the handle click')
+    let prevState = this.state.isFave;
+    this.setState ({
+      isFave: !prevState
+    })
     e.stopPropagation(e)
   }
   render() {
+    let classToggle = this.state.isFave ? "remove_from_queue" : "add_to_queue"
+
+     
     return (
-      <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
+      <div className={`film-row-fave ${classToggle}`} onClick={this.handleClick}>
         <p className="material-icons">add_to_queue</p>
       </div>
     )
