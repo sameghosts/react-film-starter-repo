854e9da42d51138fdf74d096a41d2606f57dd92d
sameghosts <seanfeiner.brown@gmail.com>
2021-02-04 00:00:23 -0800

    state change class name changes

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -19,18 +19,18 @@ class FilmListing extends Component {
   render() {
     let allFilms=this.props.films.map((film, i) => {
       return (
-       <FilmRow film={film} key={`film-row-${i}`} />
+        <FilmRow film={film} key={`film-row-${i}`} />
       )
     })
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
         <div className="film-list-filters">
-          <div className="film-list-filter" onClick={()=> this.handleFilterClick('all')}>
+          <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={()=> { this.handleFilterClick('all')}}>
             ALL 
             <span className="section-count">{this.props.films.length}</span>
           </div>
-            <div className="film-list-filter" onClick={()=>this.handleFilterClick('faves')}>
+            <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={()=> {this.handleFilterClick('faves')}}>
               FAVES
               <span className="section-count">0</span>
             </div>
